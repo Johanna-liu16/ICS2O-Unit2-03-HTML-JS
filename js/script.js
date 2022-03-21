cde5334a71d6cd2f5b695d7b0d30ed464aaaa32f
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-03-21 17:29:11 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@
 // Created by: Johanna Liu
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users street number and street name and shows it back to user.
+ */
+function myButtonClicked() {
+  // input
+  const streetNumber = document.getElementById("street-number").value
+  const streetName = document.getElementById("street-name").value
+
+  // output
+  document.getElementById("address").innerHTML = 
+    "<p>Your address is: " streetNumber + streetName ".</p>";
+}
